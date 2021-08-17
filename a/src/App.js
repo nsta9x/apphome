0bca51590c829dce059f8cb7fb07eb3fe7d39df5
ngocson.ta <ngocson.ta.astek@axa.fr>
2021-08-17 21:59:05 +0200

    new pj

---
+++ a/src/App.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { createAppContainer } from 'react-navigation';
+import SmartHome from './navigation/SmartHome';
+
+export default createAppContainer(SmartHome);
